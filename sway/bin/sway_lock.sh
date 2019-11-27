c8f95b2dc8db28495d079bdedc4d6052799f4689
Christian Hernvall <christian.hernvall@gmail.com>
2019-11-27 20:31:57 +0100

    sway: Update lockscreen

--- sway/bin/sway_lock.sh
+++ sway/bin/sway_lock.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-grim /tmp/screen_lock.png
-convert /tmp/screen_lock.png -scale 1% -scale 10000% /tmp/screen_lock.png
-swaylock -i /tmp/screen_lock.png
+# grim /tmp/screen_lock.png
+# convert /tmp/screen_lock.png -scale 1% -scale 10000% /tmp/screen_lock.png
+# swaylock -i /tmp/screen_lock.png
+
+
+outputs=$(swaymsg -t get_outputs | jq -r .[].name)
+image=$(find ~/phippy/ -name '*.png' -or -name '*.jpg' | shuf -n1)
+lock_args=""
+for output in ${outputs}; do
+    lock_args="${lock_args} -i ${output}:${image}"
+done
+swaylock ${lock_args}
