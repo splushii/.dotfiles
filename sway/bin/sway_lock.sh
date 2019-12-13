c15559a0b8767c2205d67b3218e6a83df807eea4
Christian Hernvall <christian.hernvall@svt.se>
2019-12-13 13:44:26 +0100

    sway: Phippy screensaver(/burner) complete

--- sway/bin/sway_lock.sh
+++ sway/bin/sway_lock.sh
@@ -3,11 +3,26 @@
 # convert /tmp/screen_lock.png -scale 1% -scale 10000% /tmp/screen_lock.png
 # swaylock -i /tmp/screen_lock.png
 
-
 outputs=$(swaymsg -t get_outputs | jq -r .[].name)
-image=$(find ~/phippy/ -name '*.png' -or -name '*.jpg' | shuf -n1)
+
+# Expects the phippy (https://www.cncf.io/phippy-goes-to-the-zoo-book/) images
+# in ~/phippy/ with the images in pairs with:
+# phippy-1-<num>.(png|jpg) for the picture
+# phippy-2-<num>.(png|jpg) for the description
+img_num=$(find ~/phippy/ -name '*.png' -or -name '*.jpg' | sed -E 's/.*phippy-[[:digit:]]+-(.*).(png|jpg)/\1/' | sort | uniq | shuf -n1)
+img_pic=$(ls ~/phippy/phippy-1-${img_num}.*)
+img_desc=$(ls ~/phippy/phippy-2-${img_num}.*)
 lock_args=""
+count=1
 for output in ${outputs}; do
-    lock_args="${lock_args} -i ${output}:${image}"
+    if (( count == 1 )); then
+        # The first screen is usually the (smaller) laptop screen
+        # Put the slightly more boring description here
+        lock_args="${lock_args} -i ${output}:${img_desc}"
+    else
+        # Show the nice picture on all other (larger) screens
+        lock_args="${lock_args} -i ${output}:${img_pic}"
+    fi
+    (( count = count + 1 ))
 done
-swaylock ${lock_args}
+swaylock --color 000000 --scaling fit ${lock_args}
