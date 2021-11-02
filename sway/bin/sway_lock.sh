89549835f248e55f81b38203ea6983ce3f5c70cf
Christian Hernvall <c.hernvall@yubico.com>
2021-11-02 09:02:30 +0100

    sway update

--- sway/bin/sway_lock.sh
+++ sway/bin/sway_lock.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+swaylock --color 000000
+exit 0
+
 # grim /tmp/screen_lock.png
 # convert /tmp/screen_lock.png -scale 1% -scale 10000% /tmp/screen_lock.png
 # swaylock -i /tmp/screen_lock.png
